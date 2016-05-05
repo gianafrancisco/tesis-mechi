c2083b20afa6eb710584441a49c539b91b860f2b
Francisco Giana <gianafrancisco@gmail.com>
2016-05-05 14:55:21 -0300

    create default admin user.

--- src/main/java/com/mechi/webservices/Application.java
+++ src/main/java/com/mechi/webservices/Application.java
@@ -1,6 +1,8 @@
 package com.mechi.webservices;
 
 
+import com.mechi.restaurant.DAO.DAO;
+import com.mechi.restaurant.modelos.Usuarios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,5 +14,15 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        if(DAO.getInstance().getUsuarioPorNombre("admin") == null){
+            Usuarios u = new Usuarios();
+            u.setEmail("admin@admin.com");
+            u.setNombre("admin");
+            u.setActivo(true);
+            u.setPassword("admin");
+            u.setRol("ADMIN");
+            DAO.getInstance().add(u);
+        }
     }
 }
